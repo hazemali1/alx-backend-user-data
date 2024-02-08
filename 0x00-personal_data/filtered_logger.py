7d86bf8a61cec2c4e530a0e57d29de786e8c8232
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-08 22:25:02 +0200

    re.sub

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,6 @@ from functools import reduce
 
 
 def filter_datum(fields, redaction, message, separator):
-    """filter datum fields"""
     li = []
     [li.append(q[1]) for q in [re.split("=", d) for d in re.split(r';', message)] if q[0] in fields]
     message = reduce(lambda m, p: re.sub(p, redaction, m), li, message)
