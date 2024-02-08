bae49a4d5da8afbea83ceb0d7bdf76a935fcadab
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-09 01:44:52 +0200

    hash password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,10 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """hash password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check if password is valid"""
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    return False
