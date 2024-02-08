ad417355a4628bc5df5193c1aa1ed9f6beb2079d
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-09 01:18:32 +0200

    db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -49,12 +49,13 @@ def get_logger() -> logging.Logger:
     return log
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """return conection to the db"""
-    u = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    p = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    h = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    n = os.getenv("PERSONAL_DATA_DB_NAME")
-    db = mysql.connector.connection.MySQLConnection(user=u, password=p,
-                                 host=h, database=n)
-    return db
+def get_db() -> mysql.connector.connection.MYSQLConnection:
+    """ Connection to MySQL environment """
+    db_connect = mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+    return db_connect
+
