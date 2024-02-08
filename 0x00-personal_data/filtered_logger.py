b20c292e7663a9e0db33e09a420a65c745d963c5
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-08 22:37:39 +0200

    re.sub

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 """logging module"""
-import logging
 import re
-from functools import reduce
 from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """filter datum fields"""
-    li = []
-    [li.append(q[1]) for q in [re.split("=", d) for d in re.split(r';', message)] if q[0] in fields]
-    message = reduce(lambda m, p: re.sub(p, redaction, m), li, message)
+    for x in fields:
+        message = re.sub(f'{x}=(.*?){separator}',
+                         f'{x}={redaction}{separator}',message)
     return message
