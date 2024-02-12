27dde75844250be5f8cc6a8b896ac45701928fc4
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-12 21:36:05 +0200

    Auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -50,7 +50,7 @@ def before_request():
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
-    if auth:
+    if auth is None:
         return
     if auth.require_auth(request.path, excluded_paths):
         return
