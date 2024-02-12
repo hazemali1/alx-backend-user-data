05b48913f0f4fc48a1482cdb01171ec0e3c014a7
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-12 21:38:33 +0200

    Auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -52,7 +52,7 @@ def before_request():
                       '/api/v1/forbidden/']
     if auth is None:
         return
-    if auth.require_auth(request.path, excluded_paths):
+    if auth.require_auth(request.path, excluded_paths) == False:
         return
     if auth.authorization_header(request) == None:
         abort(401)
