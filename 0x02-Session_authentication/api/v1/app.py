b98f82690cc8540897e949b6a1e2d02c7aad465c
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-15 00:47:09 +0200

    user me

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -61,8 +61,10 @@ def before_request():
         return
     if auth.require_auth(request.path, excluded_paths) is False:
         return
+    if auth.session_cookie(request):
+        return auth.session_cookie(request)
     if auth.authorization_header(request) is None:
-        abort(403)
+        abort(401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
