0418c60f7a153e804a22678ac4a02a5e9bbb4943
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-14 17:33:50 +0200

    user me

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,7 +3,17 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """session auth class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create session method"""
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
