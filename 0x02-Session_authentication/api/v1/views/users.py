69173b6c107d7194218ae710ad6a49a55561166c
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-14 16:55:21 +0200

    user me

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -29,7 +29,7 @@ def view_one_user(user_id: str = None) -> str:
         abort(404)
     user = User.get(user_id)
     if user is None:
-        abort(404)
+        abort(401)
     if user_id == "me" and request.current_user is None:
         abort(404)
     if user_id == "me" and request.current_user is not None:
