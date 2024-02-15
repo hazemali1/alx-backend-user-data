2ddc0233c3dc4e908519fd195aae4ef6dacd9b1b
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-15 02:22:00 +0200

    session

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -14,20 +14,18 @@ def auth_session_login():
       - the auth_session of the API
     """
     from api.v1.app import auth
-
-
     email = request.form.get('email')
     password = request.form.get('password')
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     obj = User()
     if len(obj.search({'email': email})) == 0:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     user = obj.search({'email': email})[0]
     if user.is_valid_password(password) is False:
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
     session_id = auth.create_session(user.id)
     js = jsonify(user.to_json())
     session_name = os.environ.get('SESSION_NAME')
