57a4758b359dc641bef221d5f0a5299ef5858af7
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-15 15:26:03 +0200

    Expiration

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
@@ -49,6 +50,8 @@ elif auth == "session_auth":
     auth = SessionAuth()
 elif auth == "session_exp_auth":
     auth = SessionExpAuth()
+elif auth == "session_db_auth":
+    auth = SessionDBAuth()
 else:
     auth = Auth()
 
---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Auth class's models"""
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+
+
+class SessionDBAuth(SessionExpAuth):
+    """session db auth class"""
+    def create_session(self, user_id=None):
+        """create session with session db auth"""
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        obj = UserSession()
+        obj.user_id = user_id
+        obj.session_id = session_id
+        obj.save()
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """user id for session id"""
+        if session_id is None:
+            return None
+        obj = UserSession()
+        li = obj.search({'session_id': session_id})
+        if len(li) == 0:
+            return None
+        s = li[0]
+        return s.get('user_id')
+
+    def destroy_session(self, request=None):
+        """destroy session from db"""
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        obj = UserSession()
+        obj = obj.search({'session_id': session_id})[0]
+        obj.remove()
+        return True
