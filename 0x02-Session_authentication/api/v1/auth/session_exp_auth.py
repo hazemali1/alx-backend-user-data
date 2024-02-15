5277d30d2eac4bd54a5308743f2bf4adbb176219
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-15 14:16:39 +0200

    Expiration

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Auth class's models"""
+from api.v1.auth.session_auth import SessionAuth
+import os
+from datetime import datetime
+
+
+class SessionExpAuth(SessionAuth):
+    """session exp auth class"""
+    def __init__(self):
+        """init session exp auth"""
+        if os.environ.get('SESSION_DURATION') is None:
+            s = 0
+        else:
+            try:
+                s = int(os.environ.get('SESSION_DURATION'))
+            except ValueError:
+                s = 0
+        self.session_duration = s
+
+    def create_session(self, user_id=None):
+        """create session with session exp auth"""
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        session_dictionary = {}
+        session_dictionary['user_id'] = user_id
+        session_dictionary['created_at'] = datetime.now()
+        self.user_id_by_session_id[session_id] = session_dictionary
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """user id for session id"""
+        if session_id is None:
+            return None
+        u_i_b_s_i = self.user_id_by_session_id.get(session_id)
+        if u_i_b_s_i is None:
+            return None
+        c_a = u_i_b_s_i.get('created_at')
+        if c_a is None:
+            return None
+        if c_a + self.session_duration < datetime.now():
+            return None
+        if self.session_duration <= 0:
+            return u_i_b_s_i.get('user_id')
