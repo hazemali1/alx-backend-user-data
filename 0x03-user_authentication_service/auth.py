020fe66a7db4d90f7815f810a50d275bf73cba8e
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-21 19:23:06 +0200

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,3 +39,14 @@ class Auth:
             return bcrypt.checkpw(password.encode(), user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """create session method"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session = _generate_uuid()
+            user.session_id = session
+            self._db._session.commit()
+            return session
+        except NoResultFound:
+            return None
