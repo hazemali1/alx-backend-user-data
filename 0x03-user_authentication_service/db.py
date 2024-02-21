076c8412c0833cfe42d44b9ed667dcd6499059b5
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-21 14:51:06 +0200

    user model

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -16,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -31,11 +31,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Adds user to database
-        Return: User Object
-        """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        """add user method"""
+        obj = User(email=email, hashed_password=hashed_password)
+        self._session.add(obj)
         self._session.commit()
-
-        return user
+        return obj
