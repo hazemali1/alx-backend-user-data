e02768ccff0da95326f92ad79c9641da78ee9533
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-02-21 18:59:20 +0200

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -25,3 +25,11 @@ class Auth:
             raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """validate login method"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
